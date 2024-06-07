df85e7eee7213f12708e5eb10bd368d8c58b62ee
mayank <mayank.dangwal2015@gmail.com>
2024-06-07 07:49:25 -0400

    modified:   assets/css/style.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,12 @@ function toggleNavMenu() {
   navMenu.classList.toggle('active');
 }
 
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
 
 /* ----- NAVIGATION BAR FUNCTION ----- */
